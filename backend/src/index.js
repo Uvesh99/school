02cd765b27636aac453fad9276dfc34fcded1614
Uvesh99 <sayaidisrar@gmail.com>
2025-03-11 21:37:01 +0530

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/schools", schoolRoutes);
-
+app.get("/",(req,res)=>{
+  res.send("Welcome to School API");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
